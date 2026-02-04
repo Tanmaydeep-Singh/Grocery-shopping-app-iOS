00e093bf7fc88b5b2ed585a71edb6ff547c8c7e6
Tanmaydeep-Singh <simarsing007@gmail.com>
2026-02-04 11:06:51 +0530

    base setup

---
+++ Grocery-shopping-app/Grocery_shopping_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_shopping_appApp.swift
+//  Grocery-shopping-app
+//
+//  Created by tanmaydeep on 04/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_shopping_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

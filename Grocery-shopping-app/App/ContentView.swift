2f22b079c223edd6a5504a6e154bb49bf9701c0d
Tanmaydeep-Singh <simarsing007@gmail.com>
2026-02-04 15:27:46 +0530

    Dev: Added TabView with AccountView and ViewModel. Also Added Utils for enums

--- Grocery-shopping-app/App/ContentView.swift
+++ Grocery-shopping-app/App/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var selectedTab: Tab = .home
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        TabView(selection: $selectedTab) {
+
+            HomeView()
+                .tabItem {
+                    Label("Home", systemImage: "house.fill")
+                }
+                .tag(Tab.home)
+
+            ExploreView()
+                .tabItem {
+                    Label("Explore", systemImage: "magnifyingglass")
+                }
+                .tag(Tab.explore)
+
+            CartView()
+                .tabItem {
+                    Label("Cart", systemImage: "cart.fill")
+                }
+                .tag(Tab.cart)
+
+            FavouriteView()
+                .tabItem {
+                    Label("Favourite", systemImage: "heart.fill")
+                }
+                .tag(Tab.favourite)
+
+            AccountView()
+                .tabItem {
+                    Label("Account", systemImage: "person.fill")
+                }
+                .tag(Tab.account)
+        }.tint(.green) 
     }
 }
 

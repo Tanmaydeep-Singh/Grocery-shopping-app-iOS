642684543ecdc9cc23d7a9761524bce762b26ef4
Tanmaydeep Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2026-02-07 11:48:05 +0530

    Revert "FEATURE: Added Favourites UI page with dummy data"

--- Grocery-shopping-app/Features/Favourite/ViewModel/FavouriteViewModel.swift
+++ Grocery-shopping-app/Features/Favourite/ViewModel/FavouriteViewModel.swift
@@ -6,59 +6,13 @@
 //
 
 import SwiftUI
-internal import Combine
 
-@MainActor
-final class FavouriteViewModel: ObservableObject {
-
-    @Published var favouriteItems: [CartItem] = []
-
-    init() {
-        loadDummyFavourites()
+struct FavouriteViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
+}
 
-    private func loadDummyFavourites() {
-        favouriteItems = [
-            CartItem(
-                id: "1",
-                productName: "Sprite Can",
-                productImageURL: "",
-                unitDescription: "325ml",
-                price: 1.50,
-                quantity: 1
-            ),
-            CartItem(
-                id: "2",
-                productName: "Diet Coke",
-                productImageURL: "",
-                unitDescription: "355ml",
-                price: 1.99,
-                quantity: 1
-            ),
-            CartItem(
-                id: "3",
-                productName: "Apple & Grape Juice",
-                productImageURL: "",
-                unitDescription: "2L",
-                price: 15.50,
-                quantity: 1
-            ),
-            CartItem(
-                id: "4",
-                productName: "Coca Cola Can",
-                productImageURL: "",
-                unitDescription: "325ml",
-                price: 4.99,
-                quantity: 1
-            ),
-            CartItem(
-                id: "5",
-                productName: "Pepsi Can",
-                productImageURL: "",
-                unitDescription: "330ml",
-                price: 4.99,
-                quantity: 1
-            )
-        ]
-    }
+#Preview {
+    FavouriteViewModel()
 }

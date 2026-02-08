199f9eeb643f31e6733af1a4f6054c79adf64de6
Tanmaydeep-Singh <simarsing007@gmail.com>
2026-02-08 15:52:21 +0530

    Dev: Added Orders Endpoing, Service and base Models

---
+++ Grocery-shopping-app/Core/Endpoints/CartEndpoints.swift
@@ -0,0 +1,18 @@
+//
+//  CartEndpoints.swift
+//  Nectar
+//
+//  Created by tanmaydeep on 08/02/26.
+//
+
+import Foundation
+
+enum CartEndpoints {
+
+    case createCart
+    case getCart
+    case addItemToCart
+    case updateCartItem
+    case removeCartItem
+    case getCartItems
+}

8d283d1e6c6b6d9b518e8e136ca42a30bc638730
rentamac <rentamac@rentamacs-Mac-mini.local>
2026-02-11 06:14:48 -0800

    DEV: Added Checkout flow to app.

---
+++ Grocery-shopping-app/Features/MainFlow/Checkout/View/DeliveryOptionsView.swift
@@ -0,0 +1,18 @@
+//
+//  DeliveryView.swift
+//  Nectar
+//
+//  Created by rentamac on 2/11/26.
+//
+
+import SwiftUI
+
+struct DeliveryOptionsView: View {
+    var body: some View {
+        Text("DeliveryOptionsView")
+    }
+}
+
+#Preview {
+    DeliveryOptionsView()
+}

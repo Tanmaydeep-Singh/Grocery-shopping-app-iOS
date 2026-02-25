49adeecf32dabc9cf768437e4da850978fec5d60
Tanmaydeep-Singh <simarsing007@gmail.com>
2026-02-25 20:46:39 +0530

    Dev: Added Delivery partner and location route on map using polyline

--- Grocery-shopping-app/Features/MainFlow/Map/View/MapView.swift
+++ Grocery-shopping-app/Features/MainFlow/Map/View/MapView.swift
@@ -2,46 +2,96 @@
 //  MapView.swift
 //  Nectar
 //
-//  Created by tanmaydeep on 25/02/26.
-//
 
 import SwiftUI
 import MapKit
 
-struct MapLocation: Identifiable {
-    let id = UUID()
-    let coordinate: CLLocationCoordinate2D
-}
-
 struct DeliveryTrackingView: View {
     
-    private let location = MapLocation(
-        coordinate: CLLocationCoordinate2D(
-            latitude: 25.22596,
-            longitude: 75.89851
+    
+    @State private var route: MKRoute?
+    @State private var showOrderSheet = true
+    
+    @State private var position = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(
+                latitude: 25.22596,
+                longitude: 75.89851
+            ),
+            span: MKCoordinateSpan(
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01
+            )
         )
     )
     
-    @State private var region = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(
-            latitude: 25.22596,
-            longitude: 75.89851
-        ),
-        span: MKCoordinateSpan(latitudeDelta: 0.01,
-                               longitudeDelta: 0.01)
+    
+     let userLocation = CLLocationCoordinate2D(
+        latitude: 25.22596,
+        longitude: 75.89851
     )
     
+     let deliveryPartnerLocation = CLLocationCoordinate2D(
+        latitude: 25.23596,
+        longitude: 75.88851
+    )
+    
+    
     var body: some View {
-        Map(
-            coordinateRegion: $region,
-            annotationItems: [location]
-        ) { item in
-            MapMarker(
-                coordinate: item.coordinate,
-                tint: .green
-            )
+        Map(position: $position) {
+            
+            // User Location
+            Marker("Your Location",
+                   coordinate: userLocation)
+                .tint(.green)
+            
+            // Delivery Partner
+            Marker("Delivery Partner",
+                   coordinate: deliveryPartnerLocation)
+                .tint(.blue)
+            
+            // Road Route
+            if let route {
+                MapPolyline(route.polyline)
+                    .stroke(.blue, lineWidth: 5)
+            }
         }
         .ignoresSafeArea()
+        .onAppear {
+            fetchRoute()
+        }
+        .sheet(isPresented: $showOrderSheet) {
+            MapOrderDetailsSheet()
+                .presentationDetents([.fraction(0.4), .medium, .large])
+                .presentationDragIndicator(.visible)
+        }
+    }
+    
+    private func fetchRoute() {
+        let request = MKDirections.Request()
+        
+        let sourcePlacemark = MKPlacemark(coordinate: deliveryPartnerLocation)
+        let destinationPlacemark = MKPlacemark(coordinate: userLocation)
+        
+        request.source = MKMapItem(placemark: sourcePlacemark)
+        request.destination = MKMapItem(placemark: destinationPlacemark)
+        
+        request.transportType = .automobile
+        
+        let directions = MKDirections(request: request)
+        
+        directions.calculate { response, error in
+            if let route = response?.routes.first {
+                self.route = route
+                
+//                withAnimation {
+//                                self.position = .rect(route.polyline.boundingMapRect)
+//                            }
+              
+            } else if let error {
+                print("Route error:", error.localizedDescription)
+            }
+        }
     }
 }
 

bb85699726a9d0514413132a6a73b79d8cb19490
Tanmaydeep-Singh <simarsing007@gmail.com>
2026-02-25 20:01:03 +0530

    Dev: Added base map

---
+++ Grocery-shopping-app/Features/MainFlow/Map/ViewModel/MapViewModel.swift
@@ -0,0 +1,30 @@
+//
+//  MapViewModel.swift
+//  Nectar
+//
+//  Created by tanmaydeep on 25/02/26.
+//
+
+
+import MapKit
+import Combine
+
+@MainActor
+final class MapViewModel: ObservableObject {
+    
+    @Published var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 26.2389, longitude: 73.0243),
+        span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+    )
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init(locationManager: LocationManager) {
+        locationManager.$userCoordinate
+            .compactMap { $0 }
+            .sink { [weak self] coordinate in
+                self?.region.center = coordinate
+            }
+            .store(in: &cancellables)
+    }
+}
